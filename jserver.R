9c9826a352a330a868ef9883390eff312f8f0d42
Chris Burke <cdburke@gmail.com>
2018-03-07 15:55:50 +0000

    update stats/jserver4r to set paths
    svn:7546 7b1320e8-e022-0410-a07a-c339c07ae202

--- jserver.R
+++ jserver.R
@@ -1,5 +1,28 @@
 # J server for R utilities
-# ensure the shared object is accessible
+# update this to define full paths to J bin folder, shared library, and profile:
+
+# typical for windows install in home
+pathbin= "c:/users/chris/j64-807/bin"
+pathdll= paste(pathbin,"/j.dll",sep="")
+pathpro= paste(pathbin,"/profile.ijs",sep="")
+
+# typical for linux install in home
+#pathbin= "/home/chris/j64-807/bin"
+#pathdll= paste(pathbin,"/libj.so",sep="")
+#pathpro= paste(pathbin,"/profile.ijs",sep="")
+
+# typical for macos install in home
+#pathbin= "/Users/chris/j64-807/bin"
+#pathdll= paste(pathbin,"/libj.dylib",sep="")
+#pathpro= paste(pathbin,"/profile.ijs",sep="")
+
+# typical for debian install
+# pathbin= "/usr/bin"
+# pathdll= "libj.so.8.07"
+# pathpro= "/etc/j/8.07/profile.ijs"
+
+# ------------------------------------------------
+# customize the following and add any enhancements as required:
 
 Jdo = function(exp) { .Call("rjdo",exp) }
 Jexec = function(exp) { .Call("rjexec",exp) }
@@ -7,16 +30,15 @@ Jget = function(name) { .Call("rjget",name) }
 Jgetr = function(name) { .Call("rjgetr") }
 Jset = function(name,value) { .Call("rjset",name,value) }
 
-# Jinit - the optional argument is the full pathname of a
-#         profile.ijs script in the J bin directory
-# e.g. Jinit("/home/elmo/j8/bin/profile.ijs")
-Jinit = function(profile="") {
+# Jinit - the optional argument determines whether the profile is loaded
+# e.g. Jinit()   same as Jinit(TRUE)
+Jinit = function(loadprofile=TRUE) {
  os = Sys.info()["sysname"]
  ex = if (os == "Windows") "dll" else if (os == "Darwin") "dylib" else "so"
  so = paste("libjserver4r",ex,sep=".")
  if (is.loaded("rjinit")) dyn.unload(so)
  dyn.load(so)
- .Call("rjinit", profile)
+ .Call("rjinit", pathbin, pathdll, pathpro, loadprofile)
 }
 
 # run J sentences until ....
@@ -28,4 +50,3 @@ Jrun = function(){
   cat(.Call("rjgetr"));
  }
 }
-
--- manifest.ijs
+++ manifest.ijs
@@ -5,12 +5,12 @@ CAPTION=: 'Call J from R'
 DESCRIPTION=: 0 : 0
 This library provides a J server for the R statistical package.
 
-For more information, see wiki page Interfaces/R/Jserver4R/Overview .
+For more information, see wiki page http://code.jsoftware.com/wiki/Interfaces/R/Jserver4R .
 )
 
 LABCATEGORY=: 'Stats'
 
-VERSION=: '1.0.2'
+VERSION=: '1.0.3'
 
 RELEASE=: 'j807'
 
