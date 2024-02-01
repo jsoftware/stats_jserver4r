f738703208064587004f80910028e3ac1759ad84
bill lam <bbill.lam@gmail.com>
2024-02-01 10:57:05 +0800

    minor

--- source/lib/base.h
+++ source/lib/base.h
@@ -23,10 +23,27 @@ typedef int   (__stdcall *JGetMType)     (J,C*,I*,I*,I*,I*);
 typedef int   (__stdcall *JSetAType)     (J,I,C*,I,C*);
 
 // array rep: flag, type, count, rank, shape ...
-typedef struct AREP {I f;I t;I c;I r;I s[1];} *A;
+typedef struct AREP {
+  I f;
+  I t;
+  I c;
+  I r;
+  I s[1];
+} *A;
 
 // boxed array rep: d,flag,d,type,d,atoms,US rank,US d,int d,shape ...
-typedef struct BREP {I d0;I f;I d1;I t;I d2;I n;US r;US d3;int d4;I s[1];} *B;
+typedef struct BREP {
+  I d0;
+  I f;
+  I d1;
+  I t;
+  I d2;
+  I n;
+  US r;
+  US d3;
+  int d4;
+  I s[1];
+} *B;
 
 // all fns callable from R are named "rjxxx"
 SEXP rjdo(SEXP);
